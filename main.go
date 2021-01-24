1270dfdac09e54711f7cdd1b47bb602e3f2931de
Brett Holman <bpholman5@gmail.com>
2021-01-24 15:31:25 -0700

    added daemon loop to main which updates the time once per second

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 	)
 
 func main(){
@@ -16,11 +17,20 @@ func main(){
 			os.Args[0])
 		os.Exit(1)
 	} else if len(os.Args) == 2 {
+		fmt.Println("updating status with user defined string:", os.Args[1])
 		s := os.Args[1]
 		d.Update(s)
 	} else {
-		// TODO: implement status bar
-		fmt.Println("not implemented")
+		fmt.Println("running buildin update loop")
+		c := make(chan string)
+		go func(c chan string) {
+			for i := range c{
+				d.Update(i)
+			}
+		}(c)
+		for {
+			c <- time.Now().Format(time.RFC1123)
+			time.Sleep(time.Second)
+		}
 	}
-
 }
